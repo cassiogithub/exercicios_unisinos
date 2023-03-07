e73a5b51b4568f680a8276e0faae0b515bee4622
Cássio Rodrigues <curriculocassio@hotmail.com>
2023-03-06 21:39:33 -0300

    ajusta convensão de nomeclatura

--- PYTHON/exercicio1.py
+++ PYTHON/exercicio1.py
@@ -1,66 +1,71 @@
-def dizer_ola(isExercício):
-    if (isExercício):
+def dizer_ola(is_exercicio):
+    if is_exercicio:
         print('\nExercício I\n')
 
     nome = input("Digite seu nome\n")
     print(f'Olá {nome}')
 
+
 def exercicio_dois():
     print('\nExercício II\n')
-    nomeJogadorA = input('Digite o nome de  1 jogador:\n')
-    idadeJogadorA = int(input('Digite a idade deste  jogador:\n'))
+    nome_jogador_a = input('Digite o nome de  1 jogador:\n')
+    idade_jogador_a = int(input('Digite a idade deste  jogador:\n'))
 
-    nomeJogadorB = input('Digite o nome de  outro jogador\n')
-    idadeJogadorB = int(input('Digite a idade deste  jogador\n'))
+    nome_jogador_b = input('Digite o nome de  outro jogador\n')
+    idade_jogador_b = int(input('Digite a idade deste  jogador\n'))
 
-    if (idadeJogadorA > idadeJogadorA):
-        print(f'Jogador mais velho é {nomeJogadorA}')
-    elif (idadeJogadorB > idadeJogadorA):
-        print(f'Jogador mais velho é {nomeJogadorB}')
+    if idade_jogador_a > idade_jogador_a:
+        print(f'Jogador mais velho é {nome_jogador_a}')
+    elif idade_jogador_b > idade_jogador_a:
+        print(f'Jogador mais velho é {nome_jogador_b}')
     else:
         print('Ambos jogadores tem a mesma idade.')
+
 
 def exercicio_tres():
     print('\nExercício III\n')
     dizer_ola(False)
 
-    idadePessoa = int(input('Digite a sua idade:\n'))
+    idade_pessoa = int(input('Digite a sua idade:\n'))
 
-    if(idadePessoa >= 18):
+    if idade_pessoa >= 18:
         print('Você é adulto\n')
-    elif(idadePessoa < 18 and idadePessoa > 13):
+    elif 18 > idade_pessoa > 13:
         print('Você é adolescente\n')
-    elif(idadePessoa >= 0 and idadePessoa <= 13):
+    elif 0 <= idade_pessoa <= 13:
         print('Você é criança\n')
-    elif (idadePessoa < 0 ):
+    elif idade_pessoa < 0:
         print('Idade inválida\n')
     else:
         print('Idade inválida\n')
 
+
 def exercicio_quatro():
     print('\nExercício IV\n')
-    valorTotal = 0
+    valor_total = 0
     for i in range(4):
         numero = float(input('Digite um valor:\n'))
-        valorTotal += numero
+        valor_total += numero
 
-    if((valorTotal % 2) == 0):
+    if (valor_total % 2) == 0:
         print('Valor par\n')
         return
 
     print('Valor Ímpar')
     return
 
+
 def exercicio_cinco():
     print('\nExercício V\n')
 
     VALOR_DEFAULT = 0
-    def verifica_negativo_positivo(valor):
-        if valor > VALOR_DEFAULT:
+
+    def verifica_negativo_positivo(valor_recebido):
+        if valor_recebido > VALOR_DEFAULT:
             print('Valor é positvo\n')
-        elif valor < VALOR_DEFAULT:
+        elif valor_recebido < VALOR_DEFAULT:
             print('Valor é negativo\n')
-        elif valor == VALOR_DEFAULT:
+        elif valor_recebido == VALOR_DEFAULT:
             print(f'Valor é igual a {VALOR_DEFAULT}\n')
         else:
             print(f'Valor informado é inválido')
@@ -68,8 +73,6 @@ def exercicio_cinco():
     for i in range(10):
         valor = int(input('Digite um valor:\n'))
         verifica_negativo_positivo(valor)
-
-
 
 # PARA TESTAR EM CONSOLE AS FUNÇÕES DESCOMENTAR OS METODOS
 # dizer_ola(True)
