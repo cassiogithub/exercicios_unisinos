e8966f298e0b5dacaea1869c5b2a5e19203fa1cd
Cássio Rodrigues <curriculocassio@hotmail.com>
2023-03-08 02:13:51 -0300

    Adiciona exericios de 1-6 da aula lab

---
+++ PYTHON/lab_1_semana_1/exercicio.py
@@ -0,0 +1,81 @@
+class Endereco:
+    def __init__(self, logradouro, cep, bairro, cidade, estado):
+        self.logradouro = logradouro
+        self.cep = cep
+        self.bairro = bairro
+        self.cidade = cidade
+        self.estado = estado
+
+
+def exercicio_um(com_retorno):
+    nome_usuario = input('Digite seu nome: \n')
+
+    if com_retorno is False:
+        print(f'Seu nome é {nome_usuario}')
+        return
+    else:
+        return nome_usuario
+
+
+def exercicio_dois():
+    nome_usuario = exercicio_um(True)
+    idade_usuario = input('Digite sua idade: \n')
+    print(f'Seu nome é {nome_usuario} e sua idade é {idade_usuario}\n')
+    return
+
+
+def exercicio_tres():
+    exercicio_um(False)
+    altura_usuario = float(input('Digite sua altura: \n'))
+    print(f'Sua altura é {altura_usuario: .2f}\n')
+    print('Obrigado pela sua parceria!')
+
+
+def exercicio_quatro():
+    nome_usuario = exercicio_um(True)
+    endereco = Endereco(
+        input('Digite o nome da sua rua:\n'),
+        input('Digite seu CEP:\n'),
+        input('Digite seu bairro:\n'),
+        input('Digite sua cidade:\n'),
+        input('Digite seu estado:\n')
+    )
+
+    print(f'Olá seu nome é: {nome_usuario} \n'
+          f'Seu endereço é: {endereco.cidade}, no estado de(o/a) {endereco.estado} \n'
+          f'Rua: {endereco.logradouro}, no CEP: {endereco.cep}')
+    return
+
+def exercicio_cinco():
+    soma = 0
+    produto = 1
+    for i in range(5):
+        valor_digitado = int(input('Digite um valor inteiro:'))
+        soma += valor_digitado
+        produto *= valor_digitado
+    print(f'A soma dos produtos é: {soma}')
+    print(f'O produto é: {produto}')
+
+def exericio_seis():
+    a = int(input('Digite um valor inteiro:'))
+    b = int(input('Digite um valor inteiro:'))
+    c = int(input('Digite um valor inteiro:'))
+    d = int(input('Digite um valor inteiro:'))
+    e = int(input('Digite um valor inteiro:'))
+
+    area_triangulo = (b * c ) / 2
+    print(f'Area de um triangulo {area_triangulo}')
+
+    perimetro_retangulo = a + b + c + d
+    print(f'Perímetro de um retêngulo {perimetro_retangulo}')
+
+    area_circulo = 3.14159 * e**2
+    print(f'Area de círculo  {area_circulo}')
+
+# exercicio_um(False)
+# exercicio_dois()
+# exercicio_tres()
+# exercicio_quatro()
+# exercicio_cinco()
+exericio_seis()
+
